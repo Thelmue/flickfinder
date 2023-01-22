eccbf7aec6014bb8ff35246578e8e6c96d113fcf
Shakir Thelmue <shakirthelmue@gmail.com>
2023-01-21 21:27:50 -0600

    Add files via upload
    main page is roughly done, next is the My List page
    
    Signed-off-by: Shakir Thelmue <shakirthelmue@gmail.com>

---
+++ src/script/app.js
@@ -0,0 +1,26 @@
+import { Style } from "css.gg"
+
+function loginMove() {
+    window.location.href = "login.html"
+}
+
+function signUpMove() {
+    window.location.href = "sign-up.html"
+}
+
+function mainUpMove() {
+    window.location.href = "main.html"
+}
+
+function reset2Move() {
+    window.location.href = "reset2.html"
+}
+
+function reset3Move() {
+    window.location.href = "reset3.html"
+}
+
+function mainMove() {
+    window.location.href = "main.html"
+}
+
